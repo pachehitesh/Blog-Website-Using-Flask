9cb2353ad84c7a729e6caa6d615ad85e8009d69f
Hitesh Pache <123492173+pachehitesh@users.noreply.github.com>
2024-05-06 09:51:03 +0530

    #2

--- Blog Website Using Flask/main.py
+++ Blog Website Using Flask/main.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
+
 @app.route("/")
-def home ():
+def home():
     return render_template('index.html')
 
+
 @app.route("/about")
-def about ():
+def about():
     return render_template('about.html')
 
+
 @app.route("/contact")
-def contact ():
-     return render_template('contact.html')
+def contact():
+    return render_template('contact.html')
+
+
 app.run(debug=True)
+
